a46ca79706dcc5cf168a537c0e0e499bfca4d2ee
KentrilDespair <Kentril.despair@gmail.com>
2018-12-24 00:52:30 +0100

    Pin definitions

--- main/definitions.ino
+++ main/definitions.ino
@@ -1,1 +1,30 @@
-as
+/**
+ * @file definitions.ino
+ * @date 23.12.2018
+ * @author Martin Smutny (xsmutn13)
+ * @brief TODO
+ * 
+ * Version: 1.0
+ */
+
+// GPIO Pin definitions
+#define PIN_D0 16
+#define PIN_D1 5
+#define PIN_D2 4
+#define PIN_D3 8
+#define PIN_D4 2
+#define PIN_D5 14
+#define PIN_D6 12
+#define PIN_D7 13
+#define PIN_D8 15
+
+// LED - pin mappings
+#define LED1 PIN_D0
+#define LED2 PIN_D1
+#define LED3 PIN_D2
+#define LED4 PIN_D3
+#define LED5 PIN_D4
+#define LED6 PIN_D5
+#define LED7 PIN_D6
+#define LED8 PIN_D7
+#define LED9 PIN_D8
--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,14 @@
+/**
+ * @file main.ino
+ * @date 23.12.2018
+ * @author Martin Smutny (xsmutn13)
+ * @brief TODO
+ * 
+ * Version: 1.0
+ */
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
