47bfb3a3addc5f3f8833204023e3bc7128da6a16
KentrilDespair <https://KentrilDespair@github.com>
2018-12-28 16:05:18 +0100

    gzip new

--- main/full/my_app.js
+++ main/full/my_app.js
@@ -9,7 +9,7 @@
  *        GET requests using AJAX (Framework7 approach)
  *    - Definitions of functions for each sequence, mainly
  *        using event handlers
- * Version: 1.2
+ * Version: 1.3
  */
 // new framework7 app
 var app = new Framework7({
