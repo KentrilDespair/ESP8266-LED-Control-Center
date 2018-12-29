a3b4932b685900d15a462e4c61b5767dae98dc58
KentrilDespair <https://KentrilDespair@github.com>
2018-12-29 03:39:36 +0100

    new version of js

--- main/min/my_app.js
+++ main/min/my_app.js
@@ -2,23 +2,23 @@ var app=new Framework7({root:'#app',name:'My App',id:'com.myapp.test',panel:{swi
 {case 0:app.request({method:"PUT",url:"/ind?led="+param1+"&state="+param2});break;case 1:app.request({method:"PUT",url:"/one?speed="+param1});break;case 2:app.request({method:"PUT",url:"/row?speed="+param1+"&pos="+param2});break;case 3:app.request({method:"PUT",url:"/col?speed="+param1+"&pos="+param2});break;case 4:app.request({method:"PUT",url:"/cir?speed="+param1+"&len="+param2+"&dir="+param3});break;case 5:app.request({method:"PUT",url:"/swp?speed="+param1});break;case 6:app.request({method:"PUT",url:"/arw?speed="+param1+"&pos="+param2});}}
 var active_elems=document.getElementsByClassName("toggle");var tgls=[];for(var i=0;i<active_elems.length;i++){tgls.push(app.toggle.get(active_elems[i]));}
 var click_arr=[0,0,0,0,0,0,0,0,0];var chbx=document.getElementsByTagName("input");for(var i=1;i<10;i++){if(chbx[i].checked){click_arr[i-1]=1;}}
-chbx[1].onclick=function(){if(chbx[1].checked){click_arr[0]=1;}else{click_arr[0]=0;}
+chbx[1].onchange=function(){if(chbx[1].checked){click_arr[0]=1;}else{click_arr[0]=0;}
 if(tgls[0].checked){send_seq(0,1,click_arr[0]);}}
-chbx[2].onclick=function(){if(chbx[2].checked){click_arr[1]=1;}else{click_arr[1]=0;}
+chbx[2].onchange=function(){if(chbx[2].checked){click_arr[1]=1;}else{click_arr[1]=0;}
 if(tgls[0].checked){send_seq(0,2,click_arr[1]);}}
-chbx[3].onclick=function(){if(chbx[3].checked){click_arr[2]=1;}else{click_arr[2]=0;}
+chbx[3].onchange=function(){if(chbx[3].checked){click_arr[2]=1;}else{click_arr[2]=0;}
 if(tgls[0].checked){send_seq(0,3,click_arr[2]);}}
-chbx[4].onclick=function(){if(chbx[4].checked){click_arr[3]=1;}else{click_arr[3]=0;}
+chbx[4].onchange=function(){if(chbx[4].checked){click_arr[3]=1;}else{click_arr[3]=0;}
 if(tgls[0].checked){send_seq(0,4,click_arr[3]);}}
-chbx[5].onclick=function(){if(chbx[5].checked){click_arr[4]=1;}else{click_arr[4]=0;}
+chbx[5].onchange=function(){if(chbx[5].checked){click_arr[4]=1;}else{click_arr[4]=0;}
 if(tgls[0].checked){send_seq(0,5,click_arr[4]);}}
-chbx[6].onclick=function(){if(chbx[6].checked){click_arr[5]=1;}else{click_arr[5]=0;}
+chbx[6].onchange=function(){if(chbx[6].checked){click_arr[5]=1;}else{click_arr[5]=0;}
 if(tgls[0].checked){send_seq(0,6,click_arr[5]);}}
-chbx[7].onclick=function(){if(chbx[7].checked){click_arr[6]=1;}else{click_arr[6]=0;}
+chbx[7].onchange=function(){if(chbx[7].checked){click_arr[6]=1;}else{click_arr[6]=0;}
 if(tgls[0].checked){send_seq(0,7,click_arr[6]);}}
-chbx[8].onclick=function(){if(chbx[8].checked){click_arr[7]=1;}else{click_arr[7]=0;}
+chbx[8].onchange=function(){if(chbx[8].checked){click_arr[7]=1;}else{click_arr[7]=0;}
 if(tgls[0].checked){send_seq(0,8,click_arr[7]);}}
-chbx[9].onclick=function(){if(chbx[9].checked){click_arr[8]=1;}else{click_arr[8]=0;}
+chbx[9].onchange=function(){if(chbx[9].checked){click_arr[8]=1;}else{click_arr[8]=0;}
 if(tgls[0].checked){send_seq(0,9,click_arr[8]);}}
 tgls[0].on('change',function(){if(!(tgls[0].checked)){return;}
 for(var i=0;i<tgls.length;i++){if(i!=0&&tgls[i].checked){tgls[i].toggle();}}
